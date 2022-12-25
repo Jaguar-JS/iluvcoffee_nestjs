6033ee85a7e287d044592f62187af8609271a752
Jaguar <117010302+Jaguar-JS@users.noreply.github.com>
2022-12-25 22:01:17 +0200

    add data in db

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,3 +1,5 @@
+//Миграция в TypeORM — это аналог файла с SQL-запросами
+// для обновления схем базы данных.
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { CoffeeEntity } from './entities/coffee.entity'
 import { CreateCoffeeDto } from './dto/create-coffee.dto'
@@ -9,12 +11,53 @@ import { Repository } from "typeorm";
 export class CoffeesService {
   constructor(
     @InjectRepository(CoffeeEntity)
-    private readonly coffeeRepository: Repository<CoffeeEntity>
+    private readonly coffeeRepository: Repository<CoffeeEntity>,
   ) {}
+  private readonly coffees: CoffeeEntity[]=
+    [
+      {
+        id: 1,
+        createdAt: new  Date("2022-12-25T15:40:21.824Z"),
+        updatedAt: new  Date("2022-12-25T16:07:38.956Z"),
+        name: "Espresso",
+        brand: "Short, strong coffee",
+        price: 5
+      },
+      {
+        id: 2,
+        createdAt: new  Date("2022-12-25T16:03:33.320Z"),
+        updatedAt: new  Date("2022-12-25T16:24:50.909Z"),
+        name: "Shipwreck Roast",
+        brand: "Buddy Brew",
+        price: 10
+      },
+      {
+        id: 3,
+        createdAt: new  Date("2022-12-25T16:04:20.995Z"),
+        updatedAt: new  Date("2022-12-25T16:04:20.995Z"),
+        name: "Raw coconut Latte",
+        brand: "Lucky Coffee",
+        price: 7
+      },
+      {
+        id: 4,
+        createdAt: new  Date("2022-12-25T16:24:15.735Z"),
+        updatedAt: new  Date("2022-12-25T16:24:15.735Z"),
+        name: "Espresso",
+        brand: "Lucky Coffee",
+        price: 5
+      }
+    ]
 
 // All coffees
   async getCoffees(): Promise<CoffeeEntity[]> {
-    return this.coffeeRepository.find()
+    return this.coffeeRepository.find(
+      {
+        order: {
+          createdAt: 'ASC'
+        }
+      }
+    )
   }
 
 //  by-id
@@ -29,9 +72,9 @@ export class CoffeesService {
   }
 
 // add coffee
-  async createCoffee(createCoffeeDto: CreateCoffeeDto): Promise<CoffeeEntity> {
-    const coffee =await this.coffeeRepository.create(createCoffeeDto);
-    return this.coffeeRepository.save(coffee);
+  async createCoffee(createCoffeeDto: CreateCoffeeDto): Promise<CoffeeEntity[]> {
+    // const coffee =await this.coffeeRepository.create(createCoffeeDto);
+    return this.coffeeRepository.save(this.coffees);
   }
 
 // update coffee
